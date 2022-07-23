92520f00e315dc50a18f5d36c47f5f062fcb2ba5
quevv <quevovan01062001@gmail.com>
2022-07-23 22:14:07 +0700

    Feat: add login function

---
+++ Ass03Solution/DataAccess/Repositories/IOrderDetailRepository.cs
@@ -0,0 +1,18 @@
+﻿using DataAccess.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repositories
+{
+    public interface IOrderDetailRepository
+    {
+        public OrderDetail GetOrderDetail(int orderId, int productId);
+        public IEnumerable<OrderDetail> GetList();
+        public void AddOrderDetail(OrderDetail orderDetail);
+        public void UpdateOrderDetail(OrderDetail orderDetail);
+        public void DeleteOrderDetail(int orderId, int productId);
+    }
+}
